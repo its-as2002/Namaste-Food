948de031ea26aed83883d21e3fe41b17ae66b2c9
Ayush Srivastava <its.as2002@gmail.com>
2024-08-26 20:13:03 +0530

    Update : Body.js
    -useState hooks implementd for filtering of resList

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,14 +1,26 @@
-import resList from "../Utils/mockData"
+import resList from "../Utils/mockData";
 import ReastrauntCard from "./ReastrauntCard";
+import { useState } from "react";
 const Body = () => {
+	const [dynamicResList, setResList] = useState(resList);
 	return (
 		<div className="mid-body">
 			<div className="search">
 				<input type="search" placeholder="Search" className="search-input" />
 				<i class="ri-search-2-line"></i>
 			</div>
+			<div className="component">
+				<button
+					className="filter-btn"
+					onClick={() => {
+						const filterList = dynamicResList.filter((res) => res.info.avgRating > 4);
+						setResList(filterList);
+					}}>
+						Top Rated Restaurants
+				</button>
+			</div>
 			<div className="card-container">
-				{resList.map((obj) => (
+				{dynamicResList.map((obj) => (
 					<ReastrauntCard key={obj.info.id} resData={obj} />
 				))}
 			</div>
